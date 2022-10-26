fede60bdb12ff7bf40d4bedf8c2c8e2c70d92399
ajaykumar302 <59527426+ajaykumar302@users.noreply.github.com>
2022-10-26 13:37:49 +0530

    foodapp

---
+++ src/Components/Menu.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+
+ const Menu = () => {
+  return (
+    <div className='menu'>
+        <label>Meal Types</label>
+        <ul>
+            <li>breakfast</li>
+            <li>brunch</li>
+            <li>lunch/dinner</li>
+            <li>snack</li>
+            <li>teatime</li>
+        </ul>
+        <label>Cuisine Types</label>
+        <ul>
+            <li>american</li>
+            <li>asian</li>
+            <li>british</li>
+            <li>caribbean</li>
+            <li>central europe</li>
+            <li>chinese</li>
+            <li>eastern europe</li>
+            <li>french</li>
+            <li>greek</li>
+            <li>indian</li>
+            <li>italian</li>
+            <li>japanese</li>
+            <li>korean</li>
+            <li>kosher</li>
+            <li>mediterranean</li>
+            <li>mexican</li>
+            <li>middle eastern</li>
+            <li>nordic</li>
+            <li>south american</li>
+            <li>south east asian</li>
+            <li>world</li>
+        </ul>
+        <label>Dish Types</label>
+        <ul>
+            <li>alcohol cocktail</li>
+            <li>biscuits and cookies</li>
+            <li>bread</li>
+            <li>cereals</li>
+            <li>condiments and sauces</li>
+            <li>desserts</li>
+            <li>drinks</li>
+            <li>egg</li>
+            <li>ice cream and custard</li>
+            <li>main course</li>
+            <li>pancake</li>
+            <li>pasta</li>
+            <li>pastry</li>
+            <li>pies and tarts</li>
+            <li>pizza</li>
+            <li>preps</li>
+            <li>preserve</li>
+            <li>salad</li>
+            <li>sandwiches</li>
+            <li>seafood</li>
+            <li>side dish</li>
+            <li>soup</li>
+            <li>special occasions</li>
+            <li>starter</li>
+            <li>sweets</li>
+        </ul>
+    </div>
+  )
+}
+export default Menu;
