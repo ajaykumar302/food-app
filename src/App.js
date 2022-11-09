6f29fef18f9c99b6f4a9740e8f12661736ee2081
ajaykumar302 <59527426+ajaykumar302@users.noreply.github.com>
2022-11-09 12:41:28 +0530

    app1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { BrowserRouter,Routes,Route } from 'react-router-dom';
 import './App.css';
-// import Switch from 'switch';
+
 import Home from './Components/Home';
-// import Login from './Components/Login';
+
 import Form from './Components/Form';
-// import Product from './Pages/Product';
+
 import Offers from './Pages/Offers';
 import Help from './Pages/Help';
 import Cart from './Pages/Cart';
-// import Card from './Pages/Card';
+
 import Product from './Pages/Product';
 import Data from './Store/Data';
 
@@ -21,7 +21,7 @@ function App() {
      
         <Routes>
         
-          {/* <Route path='/' element={<Login/>}/> */}
+       
           <Route path='/' element={<Form/>}/>
           <Route path='/home' element={<Home/>}/>
         
@@ -29,7 +29,7 @@ function App() {
           <Route path='/help' element={<Help/>}/>
           <Route path='/cart' element={<Cart/>}/>
           <Route path='/product' element={<Product/>}/>
-          {/* <Route path='/:category/:id' element={<Card/>} /> */}
+        
           
         
         </Routes>
